b28d09300c2f5a7aee87fe5db186696570ce8d7e
Muhammad Waleed Ahmed <mwaleedahmed256@gmail.com>
2025-09-13 22:54:26 +0500

    Update vite.config.ts
    added base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: 'https://waleed-portfolio.lovable.app/',
   server: {
     host: "::",
     port: 8080,
